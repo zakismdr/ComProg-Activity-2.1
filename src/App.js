1a05f7ed414959bce3157a84106dfb9fe562bdec
Zaki <zakismdr@gmail.com>
2024-05-21 23:51:31 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, this is me - Zaki!</h1>
+        <p>Right now, I am 20 years old and I live in Bandung City.</p>
+        <p>My full name is Muhamad Zaki Samudra and I am a College Student at Padjadjaran University.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
